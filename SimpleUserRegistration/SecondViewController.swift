e4d1f4a41d2ed16ffcfc0e5f66b71eab20d7108d
Peter <peter.zeman@procus.com>
2017-06-28 10:16:56 +0200

    Initial Commit

---
+++ SimpleUserRegistration/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SimpleUserRegistration
+//
+//  Created by Peter Zeman on 28.6.17.
+//  Copyright © 2017 Procus s.r.o. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
